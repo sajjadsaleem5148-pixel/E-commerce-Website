9674582e62c9461c26c3f6b6afa1073c956bf91d
Sajjad <sajjadsaleem5148@gmail.com>
2026-08-18 15:14:07 +0500

    Prepare backend for deployment

--- index/index.js
+++ index/index.js
@@ -29,6 +29,7 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
